c574e750df8e7bd193a9d9f15d9fb7d32075c9a2
Haowei Huang <haowei.huang@outlook.com>
2025-04-22 21:16:51 -0400

    Added a responsive footer

--- src/Components/MainWebsite/WelcomePage.js
+++ src/Components/MainWebsite/WelcomePage.js
@@ -1,41 +1,47 @@
-import { Paper, Stack, Typography, Box } from "@mui/material"
+import { Paper, Stack, Typography, Box, Container } from "@mui/material"
 import SearchBar from "../SearchHotelContext/SearchBar";
+import React from "react";
 
 export const WelcomePage = () => {
     return (
-        <Box
-            sx={{
-                position: 'relative',
-                backgroundColor: 'grey.800',
-                color: '#fff',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-                backgroundImage: `url("https://r-xx.bstatic.com/xdata/images/xphoto/2880x868/313564245.jpeg?k=c677d4c63f8a8218d275614559b8ccd5dc5f169b44667c3ff46328091c225b13&o=")`,
-                height: "30vh"
-            }}>
-            <Stack spacing={2} sx={{
-                position: 'relative',
-                p: { xs: 3, md: 6 },
-                pr: { md: 0 },
-            }}>
-                <Typography component="h1" variant="h3" color="inherit" gutterBottom>
-                    A dream stay
-                    for a bucket list trip
+        <React.Fragment>
+            <Box
+                component="main"
+                sx={{
+                    backgroundColor: 'grey.800',
+                    color: '#fff',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
+                    backgroundImage: `url("https://r-xx.bstatic.com/xdata/images/xphoto/2880x868/313564245.jpeg?k=c677d4c63f8a8218d275614559b8ccd5dc5f169b44667c3ff46328091c225b13&o=")`,
+                    height: "40%"
+                }}>
+                <Stack spacing={3} sx={{
+                    position: 'relative',
+                    p: { xs: 3, md: 6 }
+                }}>
+                    <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                        A dream stay
+                        for a bucket list trip
+                    </Typography>
+                    <Typography variant="h5" color="inherit" paragraph>
+                        Make it a trip to remember in a holiday home
+                    </Typography>
+                    <Box>
+                        <SearchBar />
+                    </Box>
+                </Stack>
+            </Box>
+            <Container maxWidth="md" sx={{ my: 4 }}>
+                <Typography>
+                    To view the admin dashboard, please create your admin account first in the database following this format:
+                    <br />
+                    {JSON.stringify({
+                        email: "[adminEmail]",
+                        role: "admin",
+                        password: "[adminPassword]"
+                    }, null, 2)}
                 </Typography>
-                <Typography variant="h5" color="inherit" paragraph>
-                    Make it a trip to remember in a holiday home
-                </Typography>
-                <SearchBar />
-            </Stack>
-            <Typography>
-                <br />
-                To view the admin dashboard, please create your admin account first in the database following this format:<br />
-                {JSON.stringify({
-                    email: "[adminEmail]",
-                    role: "admin",
-                    password: "[adminPassword]"
-                }).replace(/\"/g, "")}
-            </Typography>
-        </Box>)
+            </Container>
+        </React.Fragment>);
 };

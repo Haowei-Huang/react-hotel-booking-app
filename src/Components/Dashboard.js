9b141eae0f2a574a527f5d9635b0a718368bd64b
Haowei Huang <haowei.huang@outlook.com>
2025-05-04 21:59:30 -0400

    Update Dashboard.js
    Now the drawer is closable

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -6,7 +6,7 @@ import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
-import { AppBar, Button, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader } from '@mui/material';
+import { Button, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader } from '@mui/material';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
 import HotelIcon from '@mui/icons-material/Hotel';
@@ -24,28 +24,111 @@ import AdminViewBookings from './AdminViewBookings/AdminViewBookings';
 import Stats from './DashboardStats/Stats';
 import { StatsContextProvider } from './DashboardStats/StatsContext';
 import { userLogout } from '../helpers/users';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import MuiAppBar from '@mui/material/AppBar';
+import { styled, useTheme } from '@mui/material/styles';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+const drawerWidth = "10%";
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })( // do not forward the open prop to the DOM element
+    ({ theme }) => ({
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: `-${drawerWidth}`, // when the drawer is closed, shift the content to the left
+        variants: [
+            {
+                props: ({ open }) => open, // only apply this style if open is true
+                style: {
+                    transition: theme.transitions.create('margin', {
+                        easing: theme.transitions.easing.easeOut,
+                        duration: theme.transitions.duration.enteringScreen,
+                    }),
+                    marginLeft: 0, // when drawer is open, no margin shift
+                },
+            },
+        ],
+    }),
+);
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    variants: [
+        {
+            props: ({ open }) => open,
+            style: {
+                width: `calc(100% - ${drawerWidth})`, // when the drawer is open, set the width to 100% minus the drawer width
+                marginLeft: `${drawerWidth}`, // when the drawer is open, shift the content to the right by the drawer width
+                transition: theme.transitions.create(['margin', 'width'], {
+                    easing: theme.transitions.easing.easeOut,
+                    duration: theme.transitions.duration.enteringScreen,
+                }),
+            },
+        },
+    ],
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
 
 export const Dashboard = () => {
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
     const dispatch = useDispatch();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(true);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
     const handleLogout = async () => {
         dispatch(logout());
         await userLogout();
     };
 
-    const drawerWidth = "12%";
-
     return (<Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="fixed"
-            sx={{ width: `calc(100% - ${drawerWidth})`, ml: `${drawerWidth}` }}>
+        <AppBar position="fixed" open={open}>
             <Toolbar >
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    sx={[
+                        { mr: 2 },
+                        open && { display: 'none' }, // hide the menu button when the drawer is open
+                    ]}
+                >
+                    <MenuIcon />
+                </IconButton>
                 <Typography
                     component="h1"
                     variant="h6"
                     color="inherit"
                     noWrap
-                    sx={{ flexGrow: 1 }}
+                    sx={{ flexGrow: 1 }} // make the title grow to fill the space
                 >
                     Dashboard
                 </Typography>
@@ -54,17 +137,22 @@ export const Dashboard = () => {
         </AppBar >
         <Drawer
             sx={{
-                width: drawerWidth,
-                flexShrink: 0,
+                width: drawerWidth, // set the width of the drawer
+                flexShrink: 0, // do not allow the drawer to shrink
                 '& .MuiDrawer-paper': {
-                    width: drawerWidth,
+                    width: drawerWidth, // set the width of the drawer paper
                     boxSizing: 'border-box',
                 },
             }}
-            variant="permanent"
+            variant="persistent"
             anchor="left"
+            open={open}
         >
-            <Toolbar />
+            <DrawerHeader>
+                <IconButton onClick={handleDrawerClose}>
+                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+            </DrawerHeader>
             <Divider />
             <List subheader={
                 <ListSubheader component="div" id="nested-list-subheader">
@@ -73,14 +161,14 @@ export const Dashboard = () => {
             }>
                 <ListItemButton component={Link} to="/">
                     <ListItemIcon>
-                        <HomeIcon />
+                        <HomeIcon color="primary" />
                     </ListItemIcon>
                     <ListItemText primary="Home Page" />
                 </ListItemButton>
                 <ListItem disablePadding>
                     <ListItemButton component={Link} to="/Dashboard">
                         <ListItemIcon>
-                            <DashboardIcon />
+                            <DashboardIcon color="primary" />
                         </ListItemIcon>
                         <ListItemText primary="Dashboard" />
                     </ListItemButton>
@@ -95,7 +183,7 @@ export const Dashboard = () => {
                 <ListItem disablePadding>
                     <ListItemButton component={Link} to="/Dashboard/ManageUsers">
                         <ListItemIcon>
-                            <SupervisedUserCircleIcon />
+                            <SupervisedUserCircleIcon color="primary" />
                         </ListItemIcon>
                         <ListItemText primary="Manage Users" />
                     </ListItemButton>
@@ -103,7 +191,7 @@ export const Dashboard = () => {
                 <ListItem disablePadding>
                     <ListItemButton component={Link} to="/Dashboard/ManageHotels">
                         <ListItemIcon>
-                            <HotelIcon />
+                            <HotelIcon color="primary" />
                         </ListItemIcon>
                         <ListItemText primary="Manage Hotels" />
                     </ListItemButton>
@@ -111,7 +199,7 @@ export const Dashboard = () => {
                 <ListItem disablePadding>
                     <ListItemButton component={Link} to="/Dashboard/ViewBookings">
                         <ListItemIcon>
-                            <PaymentIcon />
+                            <PaymentIcon color="primary" />
                         </ListItemIcon>
                         <ListItemText primary="View Bookings" />
                     </ListItemButton>
@@ -119,7 +207,7 @@ export const Dashboard = () => {
                 <ListItem disablePadding>
                     <ListItemButton component={Link} to="/Dashboard">
                         <ListItemIcon>
-                            <ShowChartIcon />
+                            <ShowChartIcon color="primary" />
                         </ListItemIcon>
                         <ListItemText primary="View Status" />
                     </ListItemButton>
@@ -134,16 +222,16 @@ export const Dashboard = () => {
                 <ListItem disablePadding>
                     <ListItemButton component={Link} to="/Dashboard/ChangePassword">
                         <ListItemIcon>
-                            <KeyIcon />
+                            <KeyIcon color="primary" />
                         </ListItemIcon>
                         <ListItemText primary="Change Password" />
                     </ListItemButton>
                 </ListItem>
             </List>
         </Drawer>
-        <Box component="main"
-            sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
-            <Toolbar />
+        <Main open={open}>
+            {/* This is the space between the AppBar and the DrawerHeader, which is the space where the content will be displayed */}
+            <DrawerHeader />
             <Routes>
                 <Route index element={<StatsContextProvider><Stats /></StatsContextProvider>} />
                 <Route path="ManageUsers" element={<ManagerUsers />} />
@@ -151,7 +239,7 @@ export const Dashboard = () => {
                 <Route path="ViewBookings/*" element={<AdminViewBookings />} />
                 <Route path="ChangePassword" element={<ChangePassword />} />
             </Routes>
-        </Box>
+        </Main>
     </Box>);
 };
 
